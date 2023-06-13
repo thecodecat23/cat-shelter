bf51c4ec9e1e09f7862a0c3f0c97393250f08871
TheCodeCat <thecodecat23@gmail.com>
2023-06-13 16:35:43 +0200

    add test project

---
+++ tests/CatsShelter.Service.UnitTests/Features/Adoption/Domain/Entities/CatTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatsShelter.Service.UnitTests.Features.Adoption.Domain.Entities;
+
+public class CatTests
+{
+
+}
