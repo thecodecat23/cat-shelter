485bb3dcf5450868ec2195aaee5b0145a27bfd53
TheCodeCat <thecodecat23@gmail.com>
2023-06-13 16:27:29 +0200

    add service project with domain

---
+++ src/CatsShelter.Service/Features/Adoption/Domain/Entities/Cat.cs
@@ -0,0 +1,25 @@
+﻿namespace CatsShelter.Service.Features.Adoption.Domain.Entities;
+
+public class Cat
+{
+    public string Id { get; private set; }
+    public string Name { get; private set; }
+    public bool IsAvailable { get; private set; }
+
+    public Cat(string id, string name)
+    {
+        Id = id;
+        Name = name;
+        IsAvailable = true;
+    }
+
+    public void RequestAdoption()
+    {
+        if (!IsAvailable)
+            throw new InvalidOperationException("Cat is not available for adoption.");
+
+        IsAvailable = false;
+    }
+
+    public void CancelAdoption() => IsAvailable = true;
+}
