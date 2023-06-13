50e457e7851eb2d340ad99b0211e0f2db27e5fb2
TheCodeCat <thecodecat23@gmail.com>
2023-06-13 17:29:01 +0200

    add ICatsRepository

---
+++ src/CatsShelter.Service/Features/Adoption/Repositories/ICatsRepository.cs
@@ -0,0 +1,9 @@
+﻿using CatsShelter.Service.Features.Adoption.Domain.Entities;
+
+namespace CatsShelter.Service.Features.Adoption.Repositories;
+
+public interface ICatsRepository
+{
+    Task<Cat> GetCatByIdAsync(string id);
+    Task<bool> UpdateCatAsync(Cat cat);
+}
