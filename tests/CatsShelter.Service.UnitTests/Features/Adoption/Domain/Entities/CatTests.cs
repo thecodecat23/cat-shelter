6b27b6fd466f584f2c9d99fd2af5513398566bff
TheCodeCat <thecodecat23@gmail.com>
2023-06-13 16:43:26 +0200

    add unit tests for cat entity

--- tests/CatsShelter.Service.UnitTests/Features/Adoption/Domain/Entities/CatTests.cs
+++ tests/CatsShelter.Service.UnitTests/Features/Adoption/Domain/Entities/CatTests.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using CatsShelter.Service.Features.Adoption.Domain.Entities;
 
 namespace CatsShelter.Service.UnitTests.Features.Adoption.Domain.Entities;
 
 public class CatTests
 {
+    private readonly IFixture _fixture;
 
+    public CatTests()
+    {
+        _fixture = new Fixture().Customize(new AutoMoqCustomization());
+    }
+
+    [Fact]
+    public void RequestAdoption_CatIsAvailable_ShouldMakeCatUnavailable()
+    {
+        // Arrange
+        var cat = _fixture.Create<Cat>();
+
+        // Act
+        cat.RequestAdoption();
+
+        // Assert
+        Assert.False(cat.IsAvailable);
+    }
+
+    [Fact]
+    public void RequestAdoption_CatIsUnavailable_ShouldThrowInvalidOperationException()
+    {
+        // Arrange
+        var cat = _fixture.Build<Cat>()
+            .Do(c => c.RequestAdoption())
+            .Create();
+
+        // Act & Assert
+        var exception = Assert.Throws<InvalidOperationException>(() => cat.RequestAdoption());
+        Assert.Equal("Cat is not available for adoption.", exception.Message);
+    }
+
+    [Fact]
+    public void CancelAdoption_CatIsUnavailable_ShouldMakeCatAvailableAgain()
+    {
+        // Arrange
+        var cat = _fixture.Build<Cat>()
+            .Do(c => c.RequestAdoption())
+            .Create();
+
+        // Act
+        cat.CancelAdoption();
+
+        // Assert
+        Assert.True(cat.IsAvailable);
+    }
 }
