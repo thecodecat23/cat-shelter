cc9c5be6ec926cf372e58b55d4cf1a217e00087d
TheCodeCat <thecodecat23@gmail.com>
2023-06-14 13:52:53 +0200

    add service interface

---
+++ src/CatsShelter.Service/Features/Adoption/Services/ICatsShelterService.cs
@@ -0,0 +1,10 @@
+﻿using CatsShelter.Service.Features.Adoption.Proto;
+
+namespace CatsShelter.Service.Features.Adoption.Services;
+
+public interface ICatsShelterService
+{
+    Task<Cats> GetAvailableCats(Empty request);
+    Task<AdoptionResponse> RequestAdoption(CatRequest request);
+    Task<AdoptionResponse> CancelAdoption(CatRequest request);
+}
