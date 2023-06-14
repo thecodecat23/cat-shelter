fe8b0414aad01773aaa2dc4c41ce537ab50dcf08
TheCodeCat <thecodecat23@gmail.com>
2023-06-14 18:19:56 +0200

    add implementation of CatsAdoptionGrpcService

--- src/CatsShelter.Service/Features/Adoption/Services/CatsAdoptionGrpcService.cs
+++ src/CatsShelter.Service/Features/Adoption/Services/CatsAdoptionGrpcService.cs
@@ -1,4 +1,5 @@
-﻿using CatsShelter.Service.Features.Adoption.Proto;
+﻿using AutoMapper;
+using CatsShelter.Service.Features.Adoption.Proto;
 using Grpc.Core;
 
 namespace CatsShelter.Service.Features.Adoption.Services;
@@ -6,26 +7,42 @@ namespace CatsShelter.Service.Features.Adoption.Services;
 public class CatsAdoptionGrpcService : CatsShelterService.CatsShelterServiceBase
 {
     private readonly ICatsAdoptionService _catsAdoptionService;
+    private readonly IMapper _mapper;
 
     public CatsAdoptionGrpcService(
-        ICatsAdoptionService catsAdoptionService
+        ICatsAdoptionService catsAdoptionService,
+        IMapper mapper
     )
     {
         _catsAdoptionService = catsAdoptionService;
-    }
-
-    public override async Task<AdoptionResponse> CancelAdoption(CatRequest request, ServerCallContext context)
-    {
-        return await _catsAdoptionService.CancelAdoption(request);
-    }
-
-    public override async Task<Cats> GetAvailableCats(Empty request, ServerCallContext context)
-    {
-        return await _catsAdoptionService.GetAvailableCats(request);
+        _mapper = mapper;
     }
 
     public override async Task<AdoptionResponse> RequestAdoption(CatRequest request, ServerCallContext context)
     {
-        return await _catsAdoptionService.RequestAdoption(request);
+        var catRequestAdoptionRequest = _mapper.Map<CatAdoptionRequest>(request);
+        
+        var catRequestAdoptionResponse = await _catsAdoptionService.RequestAdoptionAsync(catRequestAdoptionRequest, context.CancellationToken);
+
+        return _mapper.Map<AdoptionResponse>(catRequestAdoptionResponse);
+    }
+
+    public override async Task<Cats> GetAvailableCats(Empty request, ServerCallContext context)
+    {        
+        var domainCats = await _catsAdoptionService.GetAvailableCatsAsync(context.CancellationToken);
+
+        return new Cats
+        {
+            Cats_ = { _mapper.Map<List<Proto.Cat>>(domainCats) }
+        };
+    }
+
+    public override async Task<AdoptionResponse> CancelAdoption(CatRequest request, ServerCallContext context)
+    {
+        var catCancelAdoptionRequest = _mapper.Map<CatAdoptionRequest>(request);
+
+        var catCancelAdoptionResponse = await _catsAdoptionService.CancelAdoptionAsync(catCancelAdoptionRequest, context.CancellationToken);
+
+        return _mapper.Map<AdoptionResponse>(catCancelAdoptionResponse);
     }
 }
--- tests/CatsShelter.Service.UnitTests/Features/Adoption/Services/CatsAdoptionGrpcServiceTests.cs
+++ tests/CatsShelter.Service.UnitTests/Features/Adoption/Services/CatsAdoptionGrpcServiceTests.cs
@@ -1,59 +1,104 @@
-﻿using CatsShelter.Service.Features.Adoption.Proto;
+﻿using AutoFixture;
+using AutoMapper;
+using CatsShelter.Service.Features.Adoption.Proto;
 using CatsShelter.Service.Features.Adoption.Services;
+using FluentAssertions;
 using Grpc.Core;
+using Grpc.Core.Testing;
+using Grpc.Core.Utils;
 using Moq;
 
 namespace CatsShelter.Service.UnitTests.Features.Adoption.Services;
 
 public class CatsAdoptionGrpcServiceTests
 {
-    private Mock<ICatsAdoptionService> _mockCatsAdoptionService;
-    private CatsAdoptionGrpcService _catsAdoptionGrpcService;
-    private Mock<ServerCallContext> _mockServerCallContext;
+    private readonly Mock<ICatsAdoptionService> _mockCatsAdoptionService;
+    private readonly Mock<IMapper> _mockMapper;
+    private readonly Fixture _fixture;
+    private readonly CatsAdoptionGrpcService _service;
+    private readonly ServerCallContext _mockContext;
 
     public CatsAdoptionGrpcServiceTests()
     {
         _mockCatsAdoptionService = new Mock<ICatsAdoptionService>();
-        _catsAdoptionGrpcService = new CatsAdoptionGrpcService(_mockCatsAdoptionService.Object);
-        _mockServerCallContext = new Mock<ServerCallContext>();
+        _mockMapper = new Mock<IMapper>();
+        _fixture = new Fixture();
+        _service = new CatsAdoptionGrpcService(_mockCatsAdoptionService.Object, _mockMapper.Object);
+
+        var tokenSource = new CancellationTokenSource();
+        _mockContext = TestServerCallContext.Create(
+            "mockMethod",
+            null,
+            DateTime.UtcNow.AddHours(1),
+            new Metadata(),
+            tokenSource.Token,
+            "mockPeer",
+            null,
+            null,
+            m => TaskUtils.CompletedTask,
+            () => new WriteOptions(),
+            writeOptions => { }
+        );
     }
 
     [Fact]
-    public async Task GetAvailableCats_ShouldCallServiceMethod()
+    public async Task RequestAdoption_Should_Call_CatsAdoptionService_And_Return_AdoptionResponse()
     {
         // Arrange
-        var request = new Empty();
+        var catRequest = _fixture.Create<CatRequest>();
+        var catAdoptionRequest = _fixture.Create<CatAdoptionRequest>();
+        var catAdoptionResponse = _fixture.Create<CatAdoptionResponse>();
+        var adoptionResponse = _fixture.Create<AdoptionResponse>();
+
+        _mockMapper.Setup(m => m.Map<CatAdoptionRequest>(catRequest)).Returns(catAdoptionRequest);
+        _mockCatsAdoptionService.Setup(s => s.RequestAdoptionAsync(catAdoptionRequest, It.IsAny<CancellationToken>())).ReturnsAsync(catAdoptionResponse);
+        _mockMapper.Setup(m => m.Map<AdoptionResponse>(catAdoptionResponse)).Returns(adoptionResponse);
 
         // Act
-        await _catsAdoptionGrpcService.GetAvailableCats(request, _mockServerCallContext.Object);
+        var response = await _service.RequestAdoption(catRequest, _mockContext);
 
         // Assert
-        _mockCatsAdoptionService.Verify(service => service.GetAvailableCats(request), Times.Once);
+        _mockCatsAdoptionService.Verify(s => s.RequestAdoptionAsync(catAdoptionRequest, It.IsAny<CancellationToken>()), Times.Once);
+        response.Should().BeEquivalentTo(adoptionResponse);
     }
 
     [Fact]
-    public async Task RequestAdoption_ShouldCallServiceMethod()
+    public async Task GetAvailableCats_Should_Call_CatsAdoptionService_And_Return_Cats()
     {
         // Arrange
-        var request = new CatRequest();
+        var domainCats = _fixture.Create<List<Service.Features.Adoption.Domain.Entities.Cat>>();
+        var protoCats = _fixture.Create<List<Service.Features.Adoption.Proto.Cat>>();
+        var expectedCats = new Cats { Cats_ = { protoCats } };
+
+        _mockCatsAdoptionService.Setup(s => s.GetAvailableCatsAsync(It.IsAny<CancellationToken>())).ReturnsAsync(domainCats);
+        _mockMapper.Setup(m => m.Map<List<Service.Features.Adoption.Proto.Cat>>(domainCats)).Returns(protoCats);
 
         // Act
-        await _catsAdoptionGrpcService.RequestAdoption(request, _mockServerCallContext.Object);
+        var response = await _service.GetAvailableCats(new Empty(), _mockContext);
 
         // Assert
-        _mockCatsAdoptionService.Verify(service => service.RequestAdoption(request), Times.Once);
+        _mockCatsAdoptionService.Verify(s => s.GetAvailableCatsAsync(It.IsAny<CancellationToken>()), Times.Once);
+        response.Should().BeEquivalentTo(expectedCats);
     }
 
     [Fact]
-    public async Task CancelAdoption_ShouldCallServiceMethod()
+    public async Task CancelAdoption_Should_Call_CatsAdoptionService_And_Return_AdoptionResponse()
     {
         // Arrange
-        var request = new CatRequest();
+        var catRequest = _fixture.Create<CatRequest>();
+        var catAdoptionRequest = _fixture.Create<CatAdoptionRequest>();
+        var catAdoptionResponse = _fixture.Create<CatAdoptionResponse>();
+        var adoptionResponse = _fixture.Create<AdoptionResponse>();
+
+        _mockMapper.Setup(m => m.Map<CatAdoptionRequest>(catRequest)).Returns(catAdoptionRequest);
+        _mockCatsAdoptionService.Setup(s => s.CancelAdoptionAsync(catAdoptionRequest, It.IsAny<CancellationToken>())).ReturnsAsync(catAdoptionResponse);
+        _mockMapper.Setup(m => m.Map<AdoptionResponse>(catAdoptionResponse)).Returns(adoptionResponse);
 
         // Act
-        await _catsAdoptionGrpcService.CancelAdoption(request, _mockServerCallContext.Object);
+        var response = await _service.CancelAdoption(catRequest, _mockContext);
 
         // Assert
-        _mockCatsAdoptionService.Verify(service => service.CancelAdoption(request), Times.Once);
+        _mockCatsAdoptionService.Verify(s => s.CancelAdoptionAsync(catAdoptionRequest, It.IsAny<CancellationToken>()), Times.Once);
+        response.Should().BeEquivalentTo(adoptionResponse);
     }
 }
