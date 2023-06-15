49cba2544088879fa799a568be7f3fb9d1ba0daa
TheCodeCat <thecodecat23@gmail.com>
2023-06-15 12:26:33 +0200

    add docker compose with mongodb

--- src/CatsShelter.Service/Program.cs
+++ src/CatsShelter.Service/Program.cs
@@ -1,15 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+using AutoMapper;
+using CatsShelter.Service.Features.Adoption.Infrastructure.Repositories;
+using CatsShelter.Service.Features.Adoption.Services;
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddAutoMapper(typeof(Program));  // Register AutoMapper
+
+
+builder.Services.AddScoped<IMapper, Mapper>();
+builder.Services.AddScoped<ICatsRepository, CatsRepository>();
+builder.Services.AddScoped<ICatsAdoptionService, CatsAdoptionService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.MapGrpcService<CatsAdoptionGrpcService>();
 
 app.Run();
