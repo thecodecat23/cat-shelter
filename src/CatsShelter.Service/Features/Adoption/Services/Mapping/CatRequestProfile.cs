ea2d9ba7b91a07b95691d1eb198e80607c8d65b9
TheCodeCat <thecodecat23@gmail.com>
2023-06-15 16:51:20 +0200

    add custom map

--- src/CatsShelter.Service/Features/Adoption/Services/Mapping/CatRequestProfile.cs
+++ src/CatsShelter.Service/Features/Adoption/Services/Mapping/CatRequestProfile.cs
@@ -6,7 +6,10 @@ public class CatRequestProfile : Profile
 {
     public CatRequestProfile()
     {
-        CreateMap<CatAdoptionRequest, Proto.CatRequest>();
-        CreateMap<Proto.CatRequest, CatAdoptionRequest>();
+        CreateMap<CatAdoptionRequest, Proto.CatRequest>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.CatId));
+
+        CreateMap<Proto.CatRequest, CatAdoptionRequest>()
+            .ForMember(dest => dest.CatId, opt => opt.MapFrom(src => src.Id));
     }
 }
