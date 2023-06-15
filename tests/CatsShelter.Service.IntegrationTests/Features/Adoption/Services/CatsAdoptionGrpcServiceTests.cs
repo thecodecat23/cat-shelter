d83808046ad6da13e1c0bc2c76da9bf3fcd193af
TheCodeCat <thecodecat23@gmail.com>
2023-06-15 16:53:50 +0200

    integration tests for grpc service

---
+++ tests/CatsShelter.Service.IntegrationTests/Features/Adoption/Services/CatsAdoptionGrpcServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoFixture;
+using CatsShelter.Service.Features.Adoption.Proto;
+using FluentAssertions;
+using Grpc.Net.Client;
+
+namespace CatsShelter.Service.IntegrationTests.Features.Adoption.Services;
+
+public class CatsAdoptionGrpcServiceTests : IClassFixture<GrpcTestFixture<Startup>>
+{
+    private readonly CatsShelterService.CatsShelterServiceClient _client;
+    private readonly Fixture _fixture;
+
+    public CatsAdoptionGrpcServiceTests(GrpcTestFixture<Startup> factory)
+    {
+        var channel = GrpcChannel.ForAddress(factory.Server.BaseAddress, new GrpcChannelOptions { HttpClient = factory.CreateClient() });
+        _client = new CatsShelterService.CatsShelterServiceClient(channel);
+        _fixture = new Fixture();
+    }
+
+    [Fact]
+    public async Task ShouldRequestAdoptionSuccessfully()
+    {
+        // Arrange
+        const string expectedMessage = "Adoption successful.";
+        var catRequest = _fixture.Create<CatRequest>();
+
+        // Act
+        var response = await _client.RequestAdoptionAsync(catRequest);
+
+        // Assert
+        response.Should()
+            .NotBeNull().And
+            .BeEquivalentTo(new AdoptionResponse
+            {
+                Success = true,
+                Message = expectedMessage
+            });
+    }
+}
---
+++ tests/CatsShelter.Service.IntegrationTests/Features/Adoption/Services/GrpcTestFixture.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Mongo2Go;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using Microsoft.Extensions.Hosting;
+
+namespace CatsShelter.Service.IntegrationTests.Features.Adoption.Services;
+
+public class GrpcTestFixture<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        var mongoDbRunner = MongoDbRunner.Start();
+        var mongoDbConnectionString = mongoDbRunner.ConnectionString;
+
+        builder.ConfigureAppConfiguration((context, config) =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                { "MongoDbConnection", mongoDbConnectionString },
+                { "DatabaseName", "TestDb" },
+                { "CollectionName", "TestCollection" }
+            });
+        })
+        .ConfigureServices(services =>
+        {
+            services.AddSingleton(mongoDbRunner);
+            services.AddScoped(provider =>
+            {
+                var mongoClient = new MongoClient(mongoDbRunner.ConnectionString);
+                return mongoClient.GetDatabase("TestDb");
+            });
+        });
+
+        var host = builder.Build();
+        host.Start();
+        return host;
+    }
+}
